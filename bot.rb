b8befdf9cf9cc5fb1b94e834cc819f935026cf92
Kingles <jmaligie@gmail.com>
2013-07-16 22:17:20 -0500

    why images, why.

--- bot.rb
+++ bot.rb
@@ -21,8 +21,8 @@ class MambleBot
 	def encodeImage(url)
 		#return Base64.encode64(url)
 		imagedata = open(url) { |io| io.read }
-		#puts imagedata
-		send "<img src='data:image/PNG;base64," + Base64.encode64(imagedata) + "'>"
+		puts imagedata
+		return "<img src='data:image/*;base64," + Base64.encode64(imagedata) + "' \\>"
 	end
 
 	def listen_commands(msg)
@@ -33,23 +33,38 @@ class MambleBot
 	end
 	
 	def roll_dice(d)
-		send Random.rand(1...d.to_i).to_s
+		if d != "1"
+			return Random.rand(1...d.to_i).to_s 
+		else
+			return "nope!"
+		end
+
 	end
 	
 	def get_pug()
 		uri = URI.parse("http://pugme.herokuapp.com/random")
 		imageurl = JSON.parse(Net::HTTP.get_response(uri).body)["pug"]
-		#encoded = encodeImage(imageurl)
-		#send encoded
-		send "<a href='"+imageurl+"'>pug</a>"
+		if Net::HTTP.get_response(URI.parse(JSON.parse(Net::HTTP.get_response(uri).body)["pug"])).body !~ /\<HTML\>/i
+			puts "encoding!!!"
+			send imageurl
+			encoded = encodeImage(imageurl)
+			return encoded
+		else
+			return "<a href='"+imageurl+"'>pug</a>"
+		end
 	end
 
 	def get_image(url)
-		send encodeImage("https://i2.wp.com/a248.e.akamai.net/assets.github.com/images/gravatars/gravatar-user-420.png")
+	#	send encodeImage("http://i2.wp.com/a248.e.akamai.net/assets.github.com/images/gravatars/gravatar-user-420.png")
+		#send encodeImage("http://images.wikia.com/jamescameronsavatar/images/1/13/Awesome.png")
+	#	send encodeImage("http://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/PNG_transparency_demonstration_2.png/280px-PNG_transparency_demonstration_2.png")
+		url = url.match(/\<.*\>(.*)\<.*\>/)[1]
+		puts url
+		send encodeImage(url)
 	end
 
 	def initialize
-		@cli = Mumble::Client.new('erulabs.com', 64738, 'MambleBot', 'qweasd')
+		@cli = Mumble::Client.new('erulabs.com', 64738, 'TheKingler', 'qweasd')
 		@cli.on_text_message do |msg|
 			if @cli.users.has_key?(msg.actor)
 				log @cli.users[msg.actor].name + ": " + msg.message
