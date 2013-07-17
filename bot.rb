13d659816d278026dfe2faec49040894ba940dea
root <root@erulabs.com>
2013-07-16 19:27:49 -0500

    things

--- bot.rb
+++ bot.rb
@@ -4,13 +4,27 @@ require 'mumble-ruby'
 require 'net/https'
 require 'uri'
 require 'json'
+require 'base64'
+require 'open-uri'
 
 class MambleBot
 
+	def log(msg)
+		File.open('/var/log/murmur/mumble.log', 'a') { |file| file.write(msg+"\n") }
+	end
+
 	def send(msg)
+		log 'Mumblebot: '+msg
 		@cli.text_channel @cli.current_channel.name, msg
 	end
 	
+	def encodeImage(url)
+		#return Base64.encode64(url)
+		imagedata = open(url) { |io| io.read }
+		#puts imagedata
+		send "<img src='data:image/PNG;base64," + Base64.encode64(imagedata) + "'>"
+	end
+
 	def listen_commands(msg)
 		case msg
 		when /pug\s?me/
@@ -24,20 +38,28 @@ class MambleBot
 	
 	def get_pug()
 		uri = URI.parse("http://pugme.herokuapp.com/random")
-		return JSON.parse(Net::HTTP.get_response(uri).body)["pug"]
+		imageurl = JSON.parse(Net::HTTP.get_response(uri).body)["pug"]
+		#encoded = encodeImage(imageurl)
+		#send encoded
+		send "<a href='"+imageurl+"'>pug</a>"
+	end
+
+	def get_image(url)
+		send encodeImage("https://i2.wp.com/a248.e.akamai.net/assets.github.com/images/gravatars/gravatar-user-420.png")
 	end
 
 	def initialize
 		@cli = Mumble::Client.new('erulabs.com', 64738, 'MambleBot', 'qweasd')
 		@cli.on_text_message do |msg|
 			if @cli.users.has_key?(msg.actor)
-				logmsg = @cli.users[msg.actor].name + ": " + msg.message
-				File.open('/var/log/murmur/mumble.log', 'a') { |file| file.write(logmsg+"\n") }
+				log @cli.users[msg.actor].name + ": " + msg.message
 				case msg.message.to_s
 				when /^(?:[\/\\]|)d(\d{1,3})$/
 					send roll_dice($1)
 				when /^m[au]mblebot/
 					listen_commands($')
+				when /^img/
+					get_image($')
 				when /spot the fed/i
 					send "NOT MUMBLEBOT. NOT MUMBLEBOT AT ALL. I didn't just log that you said #{msg.message.to_s} at all.. >_>;;. Nobody is listening."
 				end
