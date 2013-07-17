7f4775072309090dc2dd1dc6a9a578b8522c6738
root <root@erulabs.com>
2013-07-17 15:12:46 -0500

    falcons commit

--- bot.rb
+++ bot.rb
@@ -37,7 +37,7 @@ class MambleBot
 		file =	File.join('audio/',"#{msg}.fifo")
 		@cli.stream_raw_audio(file) if File.exist? file
 		sleep(1)
-		send "Available sounds: 007, bananas, calmtits, camera, csbro, dawg, eru, fwizard, hello, heyguys, kkk, lollipop, omg, pants, upcarrot, words, wow." if msg == ""
+		send "Available sounds: 007, bananas, calmtits, camera, csbro, dawg, eru, ftt, fwizard, hello, heyguys, kkk, lollipop, omg, pants, upcarrot, words, wow." if msg == ""
 	end
 			
 	
@@ -73,18 +73,11 @@ class MambleBot
 	end
 
 	def initialize
-<<<<<<< HEAD
 		@cli = Mumble::Client.new('erulabs.com', 64738, 'FalconBot', 'qweasd')
-=======
-		username = File.open("./username") { |io| io.read }
-		@cli = Mumble::Client.new('erulabs.com', 64738, username, 'qweasd')
->>>>>>> fa51318df8975cac15010f0c296a40100e44a417
 		@cli.on_text_message do |msg|
 			if @cli.users.has_key?(msg.actor)
 				log @cli.users[msg.actor].name + ": " + msg.message
 				case msg.message.to_s
-				when /fortune/
-					send `fortune`
 				when /^(?:[\/\\]|)d(\d{1,3})$/
 					send roll_dice($1)
 				when /^m[au]mblebot/
@@ -101,13 +94,8 @@ class MambleBot
 		@cli.connect
 		#@cli.mute
 		#@cli.deafen
-<<<<<<< HEAD
 		sleep(1)
 		@cli.join_channel('Team 1')
-=======
-		sleep(2)
-		@cli.join_channel('peench peench')
->>>>>>> fa51318df8975cac15010f0c296a40100e44a417
 		puts 'Press enter to terminate script';
 		gets
 		@cli.disconnect
